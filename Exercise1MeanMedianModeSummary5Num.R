d5c32b16b38443edb378093d2883de9b9ac4851f
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-04-19 23:01:56 +0200

    Calculating Median (with and without the use of the function available in the lang)

--- Exercise1MeanMedianModeSummary5Num.R
+++ Exercise1MeanMedianModeSummary5Num.R
@@ -1,7 +1,17 @@
 df <- read.csv("2016_clean.csv")
 head(df)
 
+N <- length(df$HS)
 # Media de Happiness Score
 mean(df$HS) # Using function
-sum(df$HS)/length(df$HS) # Calculating
+sum(df$HS)/N # Manually calculating
 
+#Mediana
+median(df$HS)
+HS_sorted <- sort(df$HS)
+
+if((N %% 2) == 0) {
+  print(paste("Mi mediana calculada es", (HS_sorted[floor((N+1)/2)] + HS_sorted[ceiling((N+1)/2)]) / 2))
+} else {
+  print(paste("Mi mediana calculada es", HS_sorted[(N+1)/2]))
+}
