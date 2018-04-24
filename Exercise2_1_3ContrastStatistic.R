72923f6b64f0598cb1e6e9cf328bb4899d2a68bc
Fernando Barbeiro <fernandoabcampos@gmail.com>
2018-04-24 16:26:05 +0200

    Validation that the p value is being defined correctly

--- Exercise2_1_3ContrastStatistic.R
+++ Exercise2_1_3ContrastStatistic.R
@@ -1,3 +1,4 @@
+df <- read.csv("2016_clean.csv")
 my.contrast <- function(x, mean_hipothesis){
   z <- (mean(x) - mean_hipothesis) / (sd(x) / sqrt(length(x)))
   print(paste("Contraste estadístico: ", z))
@@ -11,4 +12,9 @@ mean(df$HS) - mean_informed
 sd(df$HS) / sqrt(length(df$HS))
 
 z = my.contrast(df$HS, mean_informed)
-2 * pt(-abs(z), df = length(df) - 1)
+
+# 2 por ser bilateral
+2 * pt(-abs(z), df = length(df$HS) - 1)
+
+
+t.test(df$HS, mu=5.0, conf.level = 0.99)
